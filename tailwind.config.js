b780dd2227750e6cfe006b16546182803e48aa68
zakharaf182 <zakharaf182@gmail.com>
2022-09-20 13:19:08 +0700

    update login

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultConfig');
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
   ],
   important: true,
   theme: {
