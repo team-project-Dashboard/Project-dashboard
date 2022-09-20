0f054bf72d52a9c6de2ec30981bcba89932d18a8
zakharaf182 <zakharaf182@gmail.com>
2022-09-20 12:16:57 +0700

    update login

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,21 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
     <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center text-gray-500">
     <div className="bg-white rounded-2xl shadow-2xl flex w-2/3 max-w-4xl">
-    <div className="tulisan w-3/5 p-5"><p>Sign in Section</p></div>
-    <div className="w-2/5 "><p>Sign up section</p></div>
+    <div className=" w-3/5 p-5">
+      <div className="text-left font-bold">
+        <span className="text-[#2563eb]">Dashboard</span>App
+      </div>
+      <div className="py-10">
+        <h2 className="text-3xl font-bold text-[#2563eb]">Sign in to Access
+        </h2>
+        <div className="border-2 w-10 bg-[#6b21a8] inline-block"></div>
+        </div>
+    </div>
+    <div className="w-2/5 rounded-tr-2xl rounded-br-2xl py-36 px-12">
+      <h2 className="text-3xl font-bold mb-2 text-[#bef264]">Welcome..!!</h2>
+      <div className="border-2 w-10 bg-[#6b21a8] inline-block"></div>
+      <p className="mb-2">student data management app</p>
+    </div>
     </div>
     </div>
     
