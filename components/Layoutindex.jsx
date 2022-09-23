0eff2ecb13729fcbd263ea455ba998d6b2d3b3a1
zakharaf182 <zakharaf182@gmail.com>
2022-09-23 07:23:35 +0700

    update

--- components/Layoutindex.jsx
+++ components/Layoutindex.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Sidebar from "./Sidebar";
+
 
 const Layoutindex = ({ children }) => {
   return (
     <div className="h-screen flex flex-row justify-start">
-      <Sidebar />
-      <div className="bg-white flex-1 p-4 text-black">
+      
+      <div className="bg-[#349CE4] flex-1 p-4 text-black">
           {children}
         </div>
       </div>
